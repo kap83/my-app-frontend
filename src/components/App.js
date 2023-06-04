c43c4d3c4fb6493703cdc23385e48a2d88886133
Karen Polanco <polanco.k@gmail.com>
2023-06-04 19:20:54 -0400

    commit message

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,6 @@
-// eslint-disable-next-line
 import React, {useState, useEffect} from 'react';
-import {Route, Routes} from "react-router-dom"
-import '../index.css';
-// eslint-disable-next-line
+import '../index.css'
 import TVShows from './TVShows';
-import NavBar from './NavBar';
-import Form from './Form'
-
-
 
 function App() {
   //eslint-disable-next-line
@@ -17,23 +10,14 @@ function App() {
   useEffect(()=> {
     fetch("http://localhost:9292/shows")
     .then(res => res.json())
-    .then(responseData => {
-      const showsData = responseData.map(show => show)
-      setShows(showsData)
-    })
+    .then( showData => setShows(showData)
+    )
   }, [])
 
-  return (
+return (
     <div className="app">
     <h1>TV Show Catalogue</h1>
-    <NavBar  />
-    <Routes>
-      <Route path='/' element={<TVShows shows={shows}/>} />
-      <Route path='/form' element={<Form />} />
-    </Routes>
-
-     
-      
+      {shows && <TVShows shows={shows} />}
     </div>
   );
 }
--- src/components/TVShows.js
+++ src/components/TVShows.js
@@ -1,15 +1,47 @@
 import React from 'react'
 // eslint-disable-next-line
-import Table from './Table';
+import AddShow from './AddShow'
 
 export default function TVShows(shows) {
 
+  console.log(shows)
+
+
     return (
-    <div>
-      {/* {shows?.map(show =>
-        <Table key={show.id}
-        show={show} 
-        />)} */}
+      <>
+      {/* <AddShow /> */}
+      
+       <div className='table-container'>
+      <table>
+        <thead>
+          <tr>
+            <th>Genre:</th>
+            <th>Title</th>
+            <th>Seasons</th>
+            <th>No of Eps</th>
+            <th>Original Language</th>
+            <th>Ongoing?</th>
+          </tr>
+        </thead>
+        <tbody>
+            {/* {shows.map((show) => {
+               return (
+                <tr key={show.id}>
+                 <td>{show.genre}</td>
+                 <td>{show.title}</td>
+                 <td>{show.seasons}</td>
+                 <td>{show.episodes}</td>
+                 <td>{show.language}</td>
+                 <td>{show.ongoing}</td>
+                 <td><button>Edit</button></td>
+                 <td><button>Delete</button></td>
+               </tr>
+               )
+            })} */}
+        </tbody> 
+      </table>
     </div>
+      </>
+   
   )
 }
